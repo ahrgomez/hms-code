60ef0b5580edee8baef5d34a4c68f587643ea0b7
Alex <alex@agtemac.local>
2017-02-21 10:56:49 +0100

    Changed literal

--- ci/src/main/java/io/ari/customers/domain/exceptions/CustomerIdCardExists.java
+++ ci/src/main/java/io/ari/customers/domain/exceptions/CustomerIdCardExists.java
@@ -3,7 +3,7 @@ package io.ari.customers.domain.exceptions;
 public class CustomerIdCardExists extends CustomerExists {
 
 	public CustomerIdCardExists(String idCard) {
-		super("existingIdCard", "Attempting to create a customer with an existing idCard:" + idCard);
+		super("existingIdCard", "Attempting to create a customer changed with an existing idCard:" + idCard);
 	}
 
 	private static final long serialVersionUID = -7651556871893634334L;
